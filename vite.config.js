32d59241d205c04411845cce57b6f4ecfff0098e
kheina <vixen@kheina.com>
2021-02-17 14:03:08 -0500

    fix upgrade to vite v2

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
+import vue from '@vitejs/plugin-vue'
 const path = require('path');
 
 export default {
+	plugins: [
+		vue(),
+	],
 	server: {
-		port: 8000,
-		hmr: false,
-	},
-	resolve: {
-		extensions: ['.js', '.vue', '.json', '.*'],
-		alias: {
-			'/@': path.resolve(__dirname, './src'),
+		port: 3000,
+		hmr: {
+			timeout: 10000,
 		},
 	},
+	// resolve: {
+	// 	extensions: ['.js', '.vue', '.json', '.*'],
+	// 	alias: {
+	// 		'/@': path.resolve(__dirname, './src'),
+	// 	},
+	// },
 }
