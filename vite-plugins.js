9afd45ca1e141ed67ac260fb0e5a1a8c3d3b7ce1
Danielle Miu <jmiu@wayfair.com>
2022-03-20 09:16:11 -0400

    fix minifier

--- vite-plugins.js
+++ vite-plugins.js
@@ -4,15 +4,12 @@ export const minifyHtml = () => {
 	return {
 		name: 'html-transform',
 		transformIndexHtml(html) {
-			return Array.from(
-				html.replace(
-					/[\t\r\n]/g, ''
-				).matchAll(
-					/<.+?'.+?'.*?>/g
-				)
-			).map(
-				match => match[0].replace(/([\w_-]+=)'(.+?)'/g, `$1"$2"`)
-			).join('');
+			return html.replace(
+				/[\t\r\n]/g, ''
+			).replace(
+				/<[^<>]+?'[^<>]+?'[^<>]*?>/g,
+				match => match.replace(/([\w_-]+=)'(.+?)'/g, `$1"$2"`)
+			);
 		}
 	};
 }
