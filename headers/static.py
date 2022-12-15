eae53e38b5fe9a2245f8e64bfb6f054b8c1224e3
kheina <vixen@kheina.com>
2022-12-14 20:38:09 -0500

    allow unsafe inline styles

--- headers/static.py
+++ headers/static.py
@@ -2,5 +2,5 @@ from typing import Dict
 
 
 Headers: Dict[str, str] = {
-	'content-security-policy': "default-src 'none'; script-src 'self'; font-src 'self'; connect-src 'self' *.fuzz.ly *.kheina.com; img-src 'self' cdn.fuzz.ly; style-src 'self'; media-src 'self'; manifest-src 'self'; style-src-attr 'self'; frame-ancestors 'none'",
+	'content-security-policy': "default-src 'none'; script-src 'self'; font-src 'self'; connect-src 'self' *.fuzz.ly *.kheina.com; img-src 'self' cdn.fuzz.ly; style-src 'unsafe-inline'; media-src 'self'; manifest-src 'self'; style-src-attr 'self'; frame-ancestors 'none'",
 }
