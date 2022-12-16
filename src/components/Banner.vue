1987da0a2e9c051b654f98187ee2980d423445eb
kheina <vixen@kheina.com>
2022-12-15 20:50:53 -0500

    fix logout with new cookie

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -292,6 +292,7 @@ export default {
 		},
 		signOut() {
 			deleteCookie('kh-auth');
+			document.cookie = `kh-auth=null; max-age=${new Date(0)}; samesite=lax; domain=.fuzz.ly; path=/; secure`;
 			this.$store.commit('setAuth', null);
 		},
 		toggleMenu() {
