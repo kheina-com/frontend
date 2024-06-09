c7f9276565f1673ac2153cc4feead83f72f96d1f
dani <29378233+DanielleMiu@users.noreply.github.com>
2024-06-09 15:56:10 -0400

    add data urls back for thumbhashes

--- headers/static.py
+++ headers/static.py
@@ -6,7 +6,7 @@ Headers: Dict[str, str] = {
 		"script-src 'self';"
 		"font-src 'self';"
 		"connect-src 'self' *.fuzz.ly api.pwnedpasswords.com;"
-		"img-src 'self' cdn.fuzz.ly blob:;"  # blob: is required for uploader and posts
+		"img-src 'self' cdn.fuzz.ly blob: data:;"  # blob: is required for uploader and posts, data: is used for thumbhashes
 		"style-src 'self' *.fuzz.ly 'unsafe-inline';"
 		"media-src 'self' cdn.fuzz.ly blob:;"
 		"manifest-src 'self';"
