cb849c5239d248b310c35fa6317674a44492f686
dani <29378233+DanielleMiu@users.noreply.github.com>
2025-02-15 15:00:21 -0500

    frontend doesn't use 3.12 yet

--- headers/static.py
+++ headers/static.py
@@ -8,31 +8,30 @@ cdn:    str = urlparse(cdn_host).netloc
 origin: str = urlparse(host).netloc
 
 
-match environment :
-	case Environment.prod :
-		Headers = {
-			'content-security-policy': "default-src 'none';"
-				"script-src 'self';"
-				"font-src 'self';"
-				f"connect-src 'self' {cdn} {origin} api.pwnedpasswords.com;"
-				f"img-src 'self' {cdn} blob: data:;"  # blob: is required for uploader and posts, data: is used for thumbhashes
-				f"style-src 'self' {origin} 'unsafe-inline';"
-				f"media-src 'self' {cdn} blob:;"
-				"manifest-src 'self';"
-				"base-uri 'self';"
-				"frame-ancestors 'none'",
-		}
+if environment.is_prod() :
+	Headers = {
+		'content-security-policy': "default-src 'none';"
+			"script-src 'self';"
+			"font-src 'self';"
+			f"connect-src 'self' {cdn} {origin} api.pwnedpasswords.com;"
+			f"img-src 'self' {cdn} blob: data:;"  # blob: is required for uploader and posts, data: is used for thumbhashes
+			f"style-src 'self' {origin} 'unsafe-inline';"
+			f"media-src 'self' {cdn} blob:;"
+			"manifest-src 'self';"
+			"base-uri 'self';"
+			"frame-ancestors 'none'",
+	}
 
-	case _ :
-		Headers = {
-			'content-security-policy': "default-src 'none';"
-				"script-src 'self';"
-				"font-src 'self';"
-				f"connect-src 'self' {cdn} {origin} api.pwnedpasswords.com;"
-				f"img-src 'self' {cdn} blob: data:;"
-				f"style-src 'self' {origin} 'unsafe-inline';"
-				f"media-src 'self' {cdn} blob:;"
-				"manifest-src 'self';"
-				"base-uri 'self';"
-				"frame-ancestors 'none'",
-		}
+else :
+	Headers = {
+		'content-security-policy': "default-src 'none';"
+			"script-src 'self';"
+			"font-src 'self';"
+			f"connect-src 'self' {cdn} {origin} api.pwnedpasswords.com;"
+			f"img-src 'self' {cdn} blob: data:;"
+			f"style-src 'self' {origin} 'unsafe-inline';"
+			f"media-src 'self' {cdn} blob:;"
+			"manifest-src 'self';"
+			"base-uri 'self';"
+			"frame-ancestors 'none'",
+	}
