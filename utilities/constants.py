99b2be14caaf6185be2cdb8e686f16e23524ddf1
dani <29378233+DanielleMiu@users.noreply.github.com>
2025-02-15 14:58:09 -0500

    frontend doesn't use 3.12 yet

--- utilities/constants.py
+++ utilities/constants.py
@@ -24,19 +24,18 @@ class Environment(Enum) :
 
 
 environment: Environment = Environment[environ.get('ENVIRONMENT', 'LOCAL').lower()]
-cdn_host: Literal['https://cdn.fuzz.ly', 'http://localhost:9000/kheina-content']
-host: Literal['http://localhost:8000', 'https://api-dev.fuzz.ly', 'https://api.fuzz.ly']
+cdn_host:    Literal['https://cdn.fuzz.ly', 'http://localhost:9000/kheina-content']
+host:        Literal['http://localhost:8000', 'https://api-dev.fuzz.ly', 'https://api.fuzz.ly']
 
 
-match environment :
-	case Environment.prod :
-		host     = 'https://api.fuzz.ly'
-		cdn_host = 'https://cdn.fuzz.ly'
+if environment.is_prod() :
+	host     = 'https://api.fuzz.ly'
+	cdn_host = 'https://cdn.fuzz.ly'
 
-	case Environment.dev :
-		host     = 'https://api-dev.fuzz.ly'
-		cdn_host = 'https://cdn.fuzz.ly'
+elif environment.is_dev() :
+	host     = 'https://api-dev.fuzz.ly'
+	cdn_host = 'https://cdn.fuzz.ly'
 
-	case _ :
-		host     = 'http://localhost:8000'
-		cdn_host = 'http://localhost:9000/kheina-content'
+else :
+	host     = 'http://localhost:8000'
+	cdn_host = 'http://localhost:9000/kheina-content'
